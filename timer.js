ede1e35b86968db958032d2186d280ab72e6fea0
kata-agata <34109598+kata-agata@users.noreply.github.com>
2020-07-10 13:29:15 +0200

    Implementing class timer - step 1, adding constructor and methods

--- timer.js
+++ timer.js
@@ -1,1 +1,26 @@
-console.log('hi');
+class Timer {
+  constructor(durationInput,startButton,pauseButton) {
+    this.durationInput = durationInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+    this.startButton.addEventListener('click', this.start);
+  }
+  start(){
+    console.log('time to start');
+  }
+  pause(){
+
+  }
+  onDurationChange(){
+
+  }
+  tick(){
+
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
