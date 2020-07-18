5d0f7901ed5f91a5be5ba849e9018edd3f955410
kata-agata <34109598+kata-agata@users.noreply.github.com>
2020-07-18 09:44:34 +0200

    Own methods added: 1. disable play button when running 2. resume from place where stopped when pause clicked 3. add restart button

--- timer.js
+++ timer.js
@@ -1,24 +1,58 @@
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const refreshButton = document.querySelector('#refresh');
 const circle = document.querySelector('circle');
 
-const perimeter = circle.getAttribute('r')*2*Math.PI;
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
 circle.setAttribute('stroke-dasharray', perimeter);
 
-let duration;
-const timer = new Timer(durationInput, startButton, pauseButton, {
-  onStart(totalDuration){ // way to communicate to outside world
-    duration = totalDuration;
+let remainingDuration =0; //time remaining in actual moment
+let startDuration = 0;//value set at start of counting time, when counter equals 0
+let counter = 0;// counter of hown many times play button was clicked, refresh()and input change resets the counter
+const timer = new Timer(durationInput, startButton, pauseButton, refreshButton, {
+  onStart(totalDuration) { // way to communicate to outside world
+    if(counter === 0){ // for first press of play button setting startDuration
+      startDuration = totalDuration;
+    }
+    remainingDuration = totalDuration;// time left to 0
+    counter++;
+    //console.log(counter);
   },
-  onTick(timeRemaining){
-    circle.setAttribute('stroke-dashoffset',
-      perimeter*timeRemaining/duration - perimeter
-  );
-
+  onRefresh() {
+    counter = 0;//reset counter
+    durationInput.value = startDuration; // starts counting from value set before
+    drawCircle(startDuration,startDuration, perimeter);//draw full circle on refresh
+  //  console.log('refresh', counter);
   },
-  onComplete(){
+  onTick(timeRemaining) {
+    if(counter <= 1){
+      drawCircle(timeRemaining,remainingDuration,perimeter); // drawing circle from the top
+    } else{
+      drawCircle(timeRemaining,startDuration, perimeter); // drawing circle where stopped when pause was clicked
+    }
+    startButton.disabled = true; //disable play button when running
+    durationInput.disabled = true;// disable input when running
+  },
+  onPause() {
+    console.log('paused');
+    startButton.disabled = false; //enable start button when pause clicked
+    durationInput.disabled = false;//enable input when pause clicked
+  },
+  onComplete() {
     console.log('timer completed');
+  },
+  onInputChange(timeRemaining){
+    console.log('input change occurs');
+    counter = 0; //reset counter
+    startDuration = timeRemaining; // setting new startDuration
+
+    drawCircle(timeRemaining,startDuration, perimeter);// circle starts from top
   }
 });
-//timer.start();//instance of timer class
+
+function drawCircle(timeRemaining, totalTime, perimeter) {
+  circle.setAttribute('stroke-dashoffset',
+    perimeter * timeRemaining / totalTime - perimeter
+  );
+}
--- timerClass.js
+++ timerClass.js
@@ -1,35 +1,42 @@
 class Timer {
-  constructor(durationInput,startButton,pauseButton, callbacks) {
+  constructor(durationInput, startButton, pauseButton, refreshButton, callbacks) {
     this.durationInput = durationInput;
     this.startButton = startButton;
     this.pauseButton = pauseButton;
-    if (callbacks){
+    this.refreshButton = refreshButton;
+    if (callbacks) {
       this.onStart = callbacks.onStart;
       this.onTick = callbacks.onTick;
       this.onComplete = callbacks.onComplete;
+      this.onPause = callbacks.onPause;
+      this.onRefresh = callbacks.onRefresh;
+      this.onInputChange = callbacks.onInputChange;
     }
 
     this.startButton.addEventListener('click', this.start);
     this.pauseButton.addEventListener('click', this.pause);
-  //  this.startButton.addEventListener('click', this.start.bind(this));
+    this.refreshButton.addEventListener('click', this.refresh);
+    this.durationInput.addEventListener('change', this.change);
 
-  /*  console.log(this);//first valid line before arrow start function
-    _defineProperty(this, "start", ()=> {
-      console.log(this);
-    }) */
+    //  this.startButton.addEventListener('click', this.start.bind(this));
+
+    /*  console.log(this);//first valid line before arrow start function
+      _defineProperty(this, "start", ()=> {
+        console.log(this);
+      }) */
 
   }
-/*  start(){
-    console.log(this);//this is a buttonElement, not what we expect
-  } */
+  /*  start(){
+      console.log(this);//this is a buttonElement, not what we expect
+    } */
 
   start = () => {
-    if(this.onStart){ //check if callbacks are given in timer
-      this.onStart(this.timeRemaining);//getter, taking start value from input, and pass to onStart().
+    if (this.onStart) { //check if callbacks are given in timer
+      this.onStart(this.timeRemaining); //getter, taking start value from input, and pass to onStart().
     }
     //  console.log(this); // solution 1. arrow function
     this.tick(); // first tick set manualy
-    this.interval = setInterval(this.tick, 50);//because this is with delay
+    this.interval = setInterval(this.tick, 50); //because this is with delay
     //timer is 1, this is id of this interval, 50ms
     //this.interval => we can refer in other functions
   };
@@ -37,31 +44,49 @@ class Timer {
     console.log(this); // solution 2. with bind
   }*/
   pause = () => {
+    if (this.onPause) {
+      this.onPause();
+    }
     clearInterval(this.interval);
   };
-  onDurationChange(){
-
-  }
+  refresh = () => {
+    if (this.onRefresh) {
+      this.onRefresh();
+    }
+    clearInterval(this.interval);
+    //this.start();
+  };
   tick = () => {
     if (this.timeRemaining <= 0) {
       this.pause();
-      if(this.onComplete){
+      if (this.onComplete) {
         this.onComplete();
       }
     } else {
-        this.timeRemaining = this.timeRemaining - .05; //setter and getter in one line
-        if(this.onTick){
-          this.onTick(this.timeRemaining); // onTick gets changed value for input
-        }
+      this.timeRemaining = this.timeRemaining - .05; //setter and getter in one line
+      if (this.onTick) {
+        this.onTick(this.timeRemaining); // onTick gets changed value for input
+      }
     }
-  //  const timeRemaining = this.timeRemaining; // dont need(), because it is getter
-  //  this.timeRemaining = timeRemaining -1;// value on rigth will be autmaticaly passed to setter
+    //  const timeRemaining = this.timeRemaining; // dont need(), because it is getter
+    //  this.timeRemaining = timeRemaining -1;// value on rigth will be autmaticaly passed to setter
   };
+  change = ()=>{
+    if(this.onInputChange){
+      this.onInputChange(this.timeRemaining);
+    }
+  }
 
-  get timeRemaining(){ // getter
+
+  get timeRemaining() {
     return parseFloat(this.durationInput.value);
   }
-  set timeRemaining(time){
+  set timeRemaining(time) {
     this.durationInput.value = time.toFixed(2); //round to 2 places after coma
   }
 }
+
+// TODO:
+//1. disable play button when running DONE
+//2. resume from place where stopped when pause clicked DONE
+//3. add restart button to start from begining DONE
